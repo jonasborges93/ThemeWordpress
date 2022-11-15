0b44569b826227d6104dd6b18361d3faff42ad72
jonasborges93 <jonas.borges93@hotmail.com>
2022-11-15 19:22:08 -0300

    Changing url icons in homepage

--- index.php
+++ index.php
@@ -2,20 +2,16 @@
 <div class="layout">
     <main class="container">
         <aside class="sidebar">
-            
-
             <?php get_template_part('template-parts/person-partial');?>
             <?php get_template_part('template-parts/technologies-partial');?>
             <?php get_template_part('template-parts/experiencies-partial');?>
             <?php get_template_part('template-parts/educations-partial');?>
-
-            
         </aside>
         <section class="content">
             <header class="content__header cards">
                 <h2 class="content__title">Meus projetos</h2>
                 <a href="#" target="_blank" class="content__link">
-                    <img src="./assets/utils/icons/github.svg" alt="Icone do GitHub" class="content__icon">
+                    <img src="<?php echo get_template_directory_uri();?>/assets/utils/icons/github.svg" alt="Icone do GitHub" class="content__icon">
                     Ver no GitHub
                 </a>
             </header>
@@ -23,7 +19,7 @@
                 <article class="project__item cards">
                     <a href="#" target="_blank" class="project__link">
                         <header class="project__header">
-                            <img src="./assets/utils/icons/folder.svg" alt="Icone de Pasta" class="project__icon">
+                            <img src="<?php echo get_template_directory_uri();?>/assets/utils/icons/folder.svg" alt="Icone de Pasta" class="project__icon">
                             <p class="project__title">Projeto de Portfolio</p>
                         </header>
                         <p class="project__text">
@@ -33,12 +29,12 @@
                         <div class="project__bellow">
                             <div class="project__info">
                                 <div class="project__infoItem">
-                                    <img src="./assets/utils/icons/star.svg" alt="Icone de Favorito"
+                                    <img src="<?php echo get_template_directory_uri();?>/assets/utils/icons/star.svg" alt="Icone de Favorito"
                                         class="project__icon--git">
                                     <p class="project__text--infoItem">100</p>
                                 </div>
                                 <div class="project__infoItem">
-                                    <img src="./assets/utils/icons/git-branch.svg" alt="Icone de Branch"
+                                    <img src="<?php echo get_template_directory_uri();?>/assets/utils/icons/git-branch.svg" alt="Icone de Branch"
                                         class="project__icon--git">
                                     <p class="project__text--infoItem">100</p>
                                 </div>
@@ -59,7 +55,7 @@
                 <article class="project__item cards">
                     <a href="#" target="_blank" class="project__link">
                         <header class="project__header">
-                            <img src="./assets/utils/icons/folder.svg" alt="Icone de Pasta" class="project__icon">
+                            <img src="<?php echo get_template_directory_uri();?>/assets/utils/icons/folder.svg" alt="Icone de Pasta" class="project__icon">
                             <p class="project__title">Projeto de Portfolio</p>
                         </header>
                         <p class="project__text">
@@ -69,12 +65,12 @@
                         <div class="project__bellow">
                             <div class="project__info">
                                 <div class="project__infoItem">
-                                    <img src="./assets/utils/icons/star.svg" alt="Icone de Favorito"
+                                    <img src="<?php echo get_template_directory_uri();?>/assets/utils/icons/star.svg" alt="Icone de Favorito"
                                         class="project__icon--git">
                                     <p class="project__text--infoItem">100</p>
                                 </div>
                                 <div class="project__infoItem">
-                                    <img src="./assets/utils/icons/git-branch.svg" alt="Icone de Branch"
+                                    <img src="<?php echo get_template_directory_uri();?>/assets/utils/icons/git-branch.svg" alt="Icone de Branch"
                                         class="project__icon--git">
                                     <p class="project__text--infoItem">100</p>
                                 </div>
@@ -95,7 +91,7 @@
                 <article class="project__item cards">
                     <a href="#" target="_blank" class="project__link">
                         <header class="project__header">
-                            <img src="./assets/utils/icons/folder.svg" alt="Icone de Pasta" class="project__icon">
+                            <img src="<?php echo get_template_directory_uri();?>/assets/utils/icons/folder.svg" alt="Icone de Pasta" class="project__icon">
                             <p class="project__title">Projeto de Portfolio</p>
                         </header>
                         <p class="project__text">
@@ -105,12 +101,12 @@
                         <div class="project__bellow">
                             <div class="project__info">
                                 <div class="project__infoItem">
-                                    <img src="./assets/utils/icons/star.svg" alt="Icone de Favorito"
+                                    <img src="<?php echo get_template_directory_uri();?>/assets/utils/icons/star.svg" alt="Icone de Favorito"
                                         class="project__icon--git">
                                     <p class="project__text--infoItem">100</p>
                                 </div>
                                 <div class="project__infoItem">
-                                    <img src="./assets/utils/icons/git-branch.svg" alt="Icone de Branch"
+                                    <img src="<?php echo get_template_directory_uri();?>/assets/utils/icons/git-branch.svg" alt="Icone de Branch"
                                         class="project__icon--git">
                                     <p class="project__text--infoItem">100</p>
                                 </div>
@@ -131,7 +127,7 @@
                 <article class="project__item cards">
                     <a href="#" target="_blank" class="project__link">
                         <header class="project__header">
-                            <img src="./assets/utils/icons/folder.svg" alt="Icone de Pasta" class="project__icon">
+                            <img src="<?php echo get_template_directory_uri();?>/assets/utils/icons/folder.svg" alt="Icone de Pasta" class="project__icon">
                             <p class="project__title">Projeto de Portfolio</p>
                         </header>
                         <p class="project__text">
@@ -141,12 +137,12 @@
                         <div class="project__bellow">
                             <div class="project__info">
                                 <div class="project__infoItem">
-                                    <img src="./assets/utils/icons/star.svg" alt="Icone de Favorito"
+                                    <img src="<?php echo get_template_directory_uri();?>/assets/utils/icons/star.svg" alt="Icone de Favorito"
                                         class="project__icon--git">
                                     <p class="project__text--infoItem">100</p>
                                 </div>
                                 <div class="project__infoItem">
-                                    <img src="./assets/utils/icons/git-branch.svg" alt="Icone de Branch"
+                                    <img src="<?php echo get_template_directory_uri();?>/assets/utils/icons/git-branch.svg" alt="Icone de Branch"
                                         class="project__icon--git">
                                     <p class="project__text--infoItem">100</p>
                                 </div>
